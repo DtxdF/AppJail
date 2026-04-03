881116b5198263047a505c14d20cf55d737a27e9
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-04-03 16:29:07 -0400

    Detect changes in Xephyr's window size and call xrandr(1)
    We simply call xrandr(1) in a loop, which isn't very efficient, though
    it doesn't consume many resources; however, I wanted to achieve
    something similar to an event system when the Xephyr window resizes,
    and now we can do that thanks to x11/xev. x11/xdotool is only needed
    to get the window id.
    
    Now, xrandr_refresh and X11_XRANDR_REFRESH are simply ignored when the
    host has x11/xdotool and x11/xev installed and the window id has been
    successfully obtained, since appjail-x11(1) will call the script
    ${SCRIPTSDIR}/xresize-exec.sh, and this will call xrandr(1) inside the
    jail only when the window's size changes.

---
+++ share/appjail/scripts/xresize-exec.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+#
+# Copyright (c) 2026, Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+set -o pipefail
+
+last_width=0
+last_height=0
+
+WINDOW_ID="$1"
+COMMAND="$2"
+
+if [ -z "${WINDOW_ID}" -o -z "${COMMAND}" ]; then
+	echo "usage: xresize-exec.sh <window id> <command> ..."
+	exit 1
+fi
+
+shift 2
+
+xev -id "${WINDOW_ID}" -event structure | while IFS= read -r line; do
+	if printf "%s" "${line}" | grep -qEe '^DestroyNotify'; then
+		kill -- -$$ > /dev/null 2>&1
+		exit 1
+	fi
+
+	wm_size=`printf "%s" "${line}" | grep -oEe ' width ([0-9]+)' -e 'height ([0-9]+)' | grep -oEe '[0-9]+'`
+
+	if [ -z "${wm_size}" ]; then
+		continue
+	fi
+
+	width=`echo ${wm_size} | cut -s -d' ' -f1`
+	height=`echo ${wm_size} | cut -s -d' ' -f2`
+
+	if [ -z "${width}" -o -z "${height}" ]; then
+		continue
+	elif [ ${width} -eq ${last_width} -a ${height} -eq ${last_height} ]; then
+		continue
+	elif [ ${width} -eq 0 -a ${height} -eq 0 ]; then
+		continue
+	fi
+
+	last_width=${width}
+	last_height=${height}
+
+	"${COMMAND}" "$@"
+
+	errlevel=$?
+
+	if [ ${errlevel} -ne 0 ]; then
+		kill -- -$$ > /dev/null 2>&1
+		exit 1
+	fi
+done || exit $?
